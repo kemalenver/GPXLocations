66edb9f28294fe12ffac90b90d6ae8fd2cf58e91
Kemal Enver <kemal.enver@sky.uk>
2016-07-14 16:27:21 +0100

    London Locations

---
+++ XCODE10Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XCODE10Test
+//
+//  Created by Kemal Enver on 14/07/2016.
+//  Copyright © 2016 Sky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
